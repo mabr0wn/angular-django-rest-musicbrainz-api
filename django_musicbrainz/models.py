3c8bb0f179048dfa9a590894acc8cdbbd8f9ab86
Matthew Brown <mattd429@gmail.com>
2017-09-15 15:17:20 -0400

    Update models.py
    added the Area model to build the db_table 'area', also added the ManyToManyField to communicate to another model.

--- django_musicbrainz/models.py
+++ django_musicbrainz/models.py
@@ -9,7 +9,7 @@ CHARACTER_VARYING_MAX_LENGTH = 10000 # Defining the max characters we can have i
 # The main annotation table which contains the actual text of the annotation, along with the changelog and the
 # identity of the editor who created it.
 class Annotation(models.Model):
-  id = models.IntergerField(primary_key=True)
+  id = models.IntegerField(primary_key=True)
   editor = models.ForeignKey('Editor', do_column='editor')
   text = models.TextField(blank=True)
   changelog = models.CharField(max_length=225, blank=True)
@@ -20,7 +20,9 @@ class Annotation(models.Model):
   class Meta:
       managed = False
       db_table = 'annotation'
-# used for external application, we have a id for identitfy this table,       
+      
+# used for external application, we have a id for identitfy this table,
+# we may not need this since we will not implement any 3rd party applications.
 class Application(models.Model):
     id = models.IntegerField(primary_key=True)
     owner = models.ForeignKey('Editor', db_column='owner')
@@ -32,4 +34,36 @@ class Application(models.Model):
     class Meta:
       managed = False
       db_table = 'application'
+
+# A country, region, city or the like.
+class Area(models.Model):
+    id = models.IntegerField(primary_key=True)
+    gid = models.TextField(unique=True)
+    name = models.CharField(max_length=CHARACTER_VARYING_MAX_LENGTH)
+    type = models.ForeignKey(
+          'AreaType', db_column='type', blank=True, null=True)
+    edits_pending = models.IntegerField()
+    last_updated = models.DateTimeField(blank=True, null=True)
+    begin_date_year = models.SmallIntegerField(blank=True, null=True)
+    begin_date_month = models.SmallIntegerField(blank=True, null=True)
+    begin_date_day = models.SmallIntegerField(blank=True, null=True)
+    end_date_year = models.SmallIntegerField(blank=True, null=True)
+    end_date_month = models.SmallIntegerField(blank=True, null=True)
+    end_date_day = models.SmallIntegerField(blank=True, null=True)
+    ended = models.BooleanField()
+    comment = models.CharField(max_length=225)
+    
+    # defing the annotation database table with area, to link to the main annotation table above.
+    
+    # manually specifiy the middle table, using through can allow django model to specify the middle table you want to use 
+    # allowing annotation model to commuicate to areaAnnotation model.
+    annotation = models.ManyToManyField(
+         'Annotation', through='AreaAnnotation')
+    areas = models.ManyToManyField('Area', through='LAreaArea')
+    edits = models.ManyToManyField('Edit', through='EditArea')
+    urls = models.ManyToManyField('Url', through='LAreaUrl')
+    
+    class Meta:
+        managed = False
+        db_table = 'area'
 
