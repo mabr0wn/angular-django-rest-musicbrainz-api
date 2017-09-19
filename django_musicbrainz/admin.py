e28df88c2d52cfcfe2ae7f6e4e113d6f80d42dcc
Matthew Brown <mattd429@gmail.com>
2017-09-19 18:13:09 -0400

    Update admin.py
    import models, import admin

--- django_musicbrainz/admin.py
+++ django_musicbrainz/admin.py
@@ -1,1 +1,7 @@
+from django.contrib import admin
+
+from .models import * # import all the models.
+
+# next we will add the register models, but that is not good programming skills to write 300 lines for admin.register.model
+# would be better to figure out a solution.
 
