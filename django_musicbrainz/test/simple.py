43d76f7a7e57403a983836341b235c20f0a9c8ee
Matthew Brown <mattd429@gmail.com>
2017-12-01 16:06:48 -0500

    Update simple.py

--- django_musicbrainz/test/simple.py
+++ django_musicbrainz/test/simple.py
@@ -1,1 +1,13 @@
 
+from django.test import runner
+
+
+class MusicbrainzTestSuiteRunner(runner.DiscoverRunner):
+
+    def setup_databases(self, **kwargs):
+        # We want to apply unit tests against the musicbrainz database
+        pass
+
+    def teardown_databases(self, old_config, **kwargs):
+        # Do not destroy our beautiful musicbrainz database
+        pass
